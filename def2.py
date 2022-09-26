196aa6569d8dfcc94831e7f2ed1ab8af745d1a7f
oluwafolabomi357 <jegedeoluwafolabomi@gmail.com>
2022-09-26 09:21:53 -0700

    added

---
+++ def2.py
@@ -0,0 +1,13 @@
+def get_input():
+    return int(input('Type a two digit number:'))
+
+def get_first digit():
+    first digit = a[0]
+    return first digit
+def get_second digit():
+    second digit = a[1]
+    return second digit
+
+
+def print_operation():
+    print_operation(first digit + second digit)
